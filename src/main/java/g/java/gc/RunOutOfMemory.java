9480c53b9483d0c598feb404f8b859038b1e0876
Galder Zamarreño <galder@zamarreno.com>
2015-09-10 10:28:58 +0200

    Chronicle example, Rx examples, composed numbers

---
+++ src/main/java/g/java/gc/RunOutOfMemory.java
@@ -0,0 +1,8 @@
+package g.java.gc;
+
+public class RunOutOfMemory {
+
+   public static void main(String[] args) throws Exception {
+
+   }
+}
--- src/main/java/g/java/rx/RxJava.java
+++ src/main/java/g/java/rx/RxJava.java
@@ -2,19 +2,20 @@ package g.java.rx;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.functions.Action1;
 
 public class RxJava {
 
    public static void main(String[] args) throws Exception {
       String[] names = {"uno", "dos", "tres"};
-      Observable.from(names).subscribe(s -> System.out.println("Hola " + s + "!"));
+      Observable.from(names).subscribe(s -> System.out.printf("[%s] Hola %s!%n", Thread.currentThread().getName(), s));
       Thread.sleep(2000);
 
       Observable.from(names).subscribe(new Observer<String>() {
          @Override
          public void onCompleted() {
-            System.out.println("Completed");
+            System.out.printf("[%s] Completed%n", Thread.currentThread().getName());
          }
 
          @Override
@@ -24,10 +25,33 @@ public class RxJava {
 
          @Override
          public void onNext(String s) {
-            System.out.println("Hola " + s + "!");
+            System.out.printf("[%s] Hola %s!%n", Thread.currentThread().getName(), s);
          }
       });
-      Thread.sleep(2000);
+      //Thread.sleep(2000);
+
+      Observable<Integer> count = Observable.from(names).count();
+      System.out.println(count.toBlocking().first());
+
+      Observable.from(names).subscribe(new Subscriber<String>() {
+         @Override
+         public void onCompleted() {
+            System.out.println("completed");
+         }
+
+         @Override
+         public void onError(Throwable e) {
+            // TODO: Customise this generated block
+         }
+
+         @Override
+         public void onNext(String s) {
+            System.out.println(s);
+            //this.unsubscribe();
+         }
+      });
+
+      //Observable.from(names).
    }
 
 }
