2a322a060422320b126150cc62eebf29fb83912d
Galder Zamarreño <galder@zamarreno.com>
2015-10-23 17:03:56 +0200

    Localhost issues

---
+++ src/main/java/g/java/net/LocalHostIssues.java
@@ -0,0 +1,24 @@
+package g.java.net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class LocalHostIssues {
+
+   public static void main(String[] args) {
+      try {
+         long start = System.currentTimeMillis();
+         InetAddress address = InetAddress.getLocalHost();
+         //String computerName = address.getHostName();
+         System.out.println(address);
+         long end = System.currentTimeMillis();
+         System.out.println("Milliseconds run: " + (end - start));
+//         if (computerName.indexOf(".") > -1)
+//            computerName = computerName.substring(0,
+//               computerName.indexOf(".")).toUpperCase();
+      } catch (UnknownHostException e) {
+         e.printStackTrace();
+      }
+   }
+
+}
