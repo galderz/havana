de12a03267bebb38580b8dff368effaa06b4fc50
Galder Zamarreño <galder@zamarreno.com>
2017-02-06 15:29:33 +0100

    More MIME type examples

--- src/main/java/g/java/mime/MimeTypes.java
+++ src/main/java/g/java/mime/MimeTypes.java
@@ -11,6 +11,12 @@ public class MimeTypes {
       System.out.println(strType.getPrimaryType());
       System.out.println(strType.getSubType());
 
+      // For String type w/ charset
+      MimeType strUtf8Type = new MimeType("text/plain;charset=UTF-8");
+      System.out.println(strUtf8Type.getPrimaryType());
+      System.out.println(strUtf8Type.getSubType());
+      System.out.println(strUtf8Type.getParameters().get("charset"));
+
       // For POJO type
       MimeType personType = new MimeType("application/x-java-object;type=g.java.mime.Person");
       System.out.println(personType.getPrimaryType());
@@ -21,6 +27,10 @@ public class MimeTypes {
       // For byte[] (java.io serialized)
       MimeType serializedType = new MimeType("application/x-java-serialized-object");
       System.out.println(serializedType);
+
+      // For normal byte[], e.g. String UTF-8 representation
+      MimeType bytesType = new MimeType("application/octet-stream");
+      System.out.println(bytesType);
    }
 
 }
