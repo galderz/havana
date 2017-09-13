1403008607fb26d84f72696d7dde44a024cd6462
Galder Zamarreño <galder@zamarreno.com>
2017-09-13 14:33:55 +0200

    Inner generics

---
+++ src/main/java/j/generics/InnerGenerics.java
@@ -0,0 +1,20 @@
+package j.generics;
+
+import java.util.function.Supplier;
+
+public class InnerGenerics {
+
+   public static void main(String[] args) {
+      Supplier<A<?>> bar = () -> new A<>();
+      //foo(bar); // <- does not compile :-(
+
+      Supplier<? extends A<?>> bar2 = () -> new A<>();
+      foo2(bar2); // compiles
+   }
+
+   static class A<T> {}
+
+   static <X> void foo(Supplier<A<X>> a) {}
+
+   static <X> void foo2(Supplier<? extends A<X>> a) {}
+}
