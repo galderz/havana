4c64b0212fb1a79e9d80da42fea8498c274b3a81
Galder Zamarreño <galder@zamarreno.com>
2018-07-03 08:14:33 +0100

    Simple class information

---
+++ src/main/java/j/classes/Classes.java
@@ -0,0 +1,11 @@
+package j.classes;
+
+public class Classes {
+
+   public static void main(String[] args) {
+      System.out.println(Void.class.getName());
+      System.out.println(Void.class.getSimpleName());
+      System.out.println(Void.class.getSimpleName().toLowerCase());
+   }
+
+}
