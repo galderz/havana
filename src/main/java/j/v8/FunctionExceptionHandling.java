a1c70d4b6a1940ef0098a9bde3d250936cad3b40
Galder Zamarreño <galder@zamarreno.com>
2018-09-27 17:45:39 +0200

    Unhappy path for function composition

--- src/main/java/j/v8/FunctionExceptionHandling.java
+++ src/main/java/j/v8/FunctionExceptionHandling.java
@@ -5,7 +5,30 @@ import java.util.function.Function;
 public class FunctionExceptionHandling {
 
    public static void main(String[] args) {
-      happyPath();
+      // Happy path calls first, second and third functions
+      // happyPath();
+
+      // If second throws an exception, third is not called
+      unhappyPath();
+   }
+
+   private static void unhappyPath() {
+      Function<String, Integer> first = s -> {
+         System.out.println("Called first");
+         return Integer.parseInt(s);
+      };
+
+      Function<Integer, Integer> second = i -> {
+         System.out.println("Called second");
+         throw new RuntimeException("boo");
+      };
+
+      Function<Integer, String> third = i -> {
+         System.out.println("Called third");
+         return String.valueOf(i);
+      };
+
+      System.out.println(first.andThen(second).andThen(third).apply("1"));
    }
 
    public static void happyPath() {
