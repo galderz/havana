679d269f91bf22a04001eb7f5ef5c1167e73536d
Galder Zamarreño <galder@zamarreno.com>
2018-10-23 14:50:00 +0200

    String format example where same parameter is printed multiple times

---
+++ src/main/java/j/string/StringFormat.java
@@ -0,0 +1,11 @@
+package j.string;
+
+public class StringFormat {
+
+   public static void main(String[] args) {
+      System.out.println(
+         String.format("%1$s %1$s %1$s %1$s %1$s %1$s", "hello")
+      );
+   }
+
+}
