f29059f067b5e2d44bd88dfc0f12b7f41561a296
Galder Zamarreño <galder@zamarreno.com>
2019-01-29 15:53:39 +0100

    Javapoet

---
+++ src/main/java/j/poet/HelloWorldPoet.java
@@ -0,0 +1,33 @@
+package j.poet;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.File;
+import java.io.IOException;
+
+public class HelloWorldPoet {
+
+   public static void main(String[] args) throws IOException {
+      MethodSpec main = MethodSpec.methodBuilder("main")
+         .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+         .returns(void.class)
+         .addParameter(String[].class, "args")
+         .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+         .build();
+
+      TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+         .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+         .addMethod(main)
+         .build();
+
+      JavaFile javaFile = JavaFile.builder("j.poet", helloWorld)
+         .build();
+
+      //javaFile.writeTo(System.out);
+      javaFile.writeTo(new File("src/main/java"));
+   }
+
+}
