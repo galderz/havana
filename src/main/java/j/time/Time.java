a526e495edbf8b3d42cb9a8d20ee1363b9d04e57
Galder Zamarreño <galder@zamarreno.com>
2019-02-05 15:30:40 +0100

    Duration timeunits and working with forever

--- src/main/java/j/time/Time.java
+++ src/main/java/j/time/Time.java
@@ -1,5 +1,7 @@
 package j.time;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class Time {
@@ -9,6 +11,12 @@ public class Time {
       System.out.println(d1);
       Date d2 = new Date(1456761614603L);
       System.out.println(d2);
+
+      System.out.println(Duration.ofSeconds(60).toNanos());
+      System.out.println(Duration.ofMillis(1).toNanos());
+
+      System.out.println(ChronoUnit.FOREVER.getDuration());
+      System.out.println(ChronoUnit.FOREVER.getDuration().equals(ChronoUnit.FOREVER.getDuration()));
    }
 
 }
