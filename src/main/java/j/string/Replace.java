8e8abc058bb01de6d70138cfe29d78274d26a97f
Galder Zamarreño <galder@zamarreno.com>
2019-02-18 12:00:37 +0100

    Basic String replace

---
+++ src/main/java/j/string/Replace.java
@@ -0,0 +1,11 @@
+package j.string;
+
+public class Replace {
+
+   public static void main(String[] args) {
+      String key = "abcd.hibernate.cache.com.acme.EntityA.memory.object.count";
+      final String replaced = key.replace("abcd.", "");
+      System.out.println(replaced);
+   }
+
+}
