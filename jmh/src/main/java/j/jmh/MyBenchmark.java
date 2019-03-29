342c548613874d7c527a18cab6d0134efd936de4
Galder Zamarreño <galder@zamarreno.com>
2019-03-29 17:17:30 +0100

    Add JMH project

---
+++ jmh/src/main/java/j/jmh/MyBenchmark.java
@@ -0,0 +1,13 @@
+package j.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class MyBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+
+}
