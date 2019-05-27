f68b74c9240f12289d9694b1b05352ed0a9036a5
Galder Zamarreño <galder@zamarreno.com>
2019-05-27 18:40:57 +0200

    Bitwise operations

---
+++ sandbox/src/main/java/j/bitwise/Bitwise.java
@@ -0,0 +1,10 @@
+package j.bitwise;
+
+public class Bitwise
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1 << 10);
+        System.out.println(1 << 5);
+    }
+}
