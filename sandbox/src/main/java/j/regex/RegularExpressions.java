8cdf40f986d401b7ff25552e6b8b22fb8534e9e0
Galder Zamarreño <galder@zamarreno.com>
2019-09-12 19:44:27 -0700

    Testing regular expressions

---
+++ sandbox/src/main/java/j/regex/RegularExpressions.java
@@ -0,0 +1,15 @@
+package j.regex;
+
+public class RegularExpressions
+{
+    public static void main(String[] args)
+    {
+        String s1 = "127.0.0.1-11223";
+        System.out.println(s1 + " -> " + replaced(s1));
+    }
+
+    static String replaced(String s) {
+        return s.replaceAll("[^\\d]", "-");
+    }
+
+}
