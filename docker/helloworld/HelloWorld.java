08673741aec5ff88f05951cd6de051ab20425a4f
Galder Zamarreño <galder@zamarreno.com>
2019-11-17 16:52:11 +0100

    Test out getting environment variables from Java

--- docker/helloworld/HelloWorld.java
+++ docker/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println(System.getenv("SITE_NAME"));
+        System.out.println(System.getenv());
     }
 }
