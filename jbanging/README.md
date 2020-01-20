c3a88d2cd94f4216df24c407b90a27f57e1c0def
Galder Zamarreño <galder@zamarreno.com>
2020-01-20 15:43:25 +0100

    Basic hello world jbang example

---
+++ jbanging/README.md
@@ -0,0 +1,8 @@
+# JBanging
+
+Trying out Max's `jbang`.
+
+```$bash
+$ cd jbanging/src
+$ jbang helloworld.java
+```
---
+++ jbanging/src/helloworld.java
@@ -0,0 +1,9 @@
+//usr/bin/env jbang "$0" "$@" ; exit $? // (1)
+
+public class helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hola mundo");
+    }
+}
