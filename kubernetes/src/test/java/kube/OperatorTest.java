939d7068b243ba47365997ee7b752f9f050677a8
Galder Zamarreño <galder@zamarreno.com>
2020-01-21 17:32:16 +0100

    Test the operator in isolation

---
+++ kubernetes/src/test/java/kube/OperatorTest.java
@@ -0,0 +1,43 @@
+package kube;
+
+import kube.Kubernetes.Event;
+import org.junit.jupiter.api.Test;
+
+import static kube.Kubernetes.EventType.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class OperatorTest
+{
+    @Test
+    public void testReconcileNoState()
+    {
+        var operator = new Operator();
+        var kube = new Kubernetes();
+        var logger = new RecordingLogger();
+        operator.reconcile(kube, logger);
+        assertThat(logger.messages.remove(), is("Namespace created"));
+    }
+
+    @Test
+    public void testReconcileNamespaceExists()
+    {
+        var operator = new Operator();
+        var kube = new Kubernetes();
+        var logger = new RecordingLogger();
+        kube.events.add(new Event("my-namespace", CREATE_NAMESPACE));
+        operator.reconcile(kube, logger);
+        assertThat(logger.messages.remove(), is("Namespace exists"));
+    }
+
+    @Test
+    public void testReconcileNamespaceError()
+    {
+        var operator = new Operator();
+        var kube = new Kubernetes();
+        var logger = new RecordingLogger();
+        kube.events.add(new Event("error", ERROR));
+        operator.reconcile(kube, logger);
+        assertThat(logger.messages.remove(), is("Namespace not created"));
+    }
+}
