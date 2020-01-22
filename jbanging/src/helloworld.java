64eb3066ca01bb76f20ec20efbf34c362ef29617
Galder Zamarreño <galder@zamarreno.com>
2020-01-22 16:44:37 +0100

    Use JAVA_HOME in jbang script

--- jbanging/src/helloworld.java
+++ jbanging/src/helloworld.java
@@ -1,9 +1,12 @@
-//usr/bin/env jbang "$0" "$@" ; exit $? // (1)
+//usr/bin/env jbang "$0" "$@" ; exit $?
+// JAVA_HOME /opt/java
+
+import java.lang.management.ManagementFactory;
 
 public class helloworld
 {
     public static void main(String[] args)
     {
-        System.out.println("hola mundo");
+        System.out.printf("hola mundo (java %s)%n", ManagementFactory.getRuntimeMXBean().getVmVersion());
     }
 }
