ca3be40e54ffebff29109c3ebb2938f75281397e
Galder Zamarreño <galder@zamarreno.com>
2020-01-27 17:56:37 +0000

    Add classpath for SubstrateVM command line call
    * Not complete since `-imagecp` missing.

--- substratevm/native-external/src/main/java/nativ/external/Main.java
+++ substratevm/native-external/src/main/java/nativ/external/Main.java
@@ -8,12 +8,55 @@ import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        var command = Arrays.asList("java", "-version");
+        final var graalVmHome = "/Users/g/.m2/repository/org/graalvm";
+        final var version = "20.1.0-SNAPSHOT";
+        final var jarSvm = String.format(
+            "%s/nativeimage/svm/%s/svm-%s.jar"
+            , graalVmHome, version, version
+        );
+        final var jarPointsTo = String.format(
+            "%s/nativeimage/pointsto/%s/pointsto-%s.jar"
+            , graalVmHome, version, version
+        );
+        final var jarSdk = String.format(
+            "%s/sdk/graal-sdk/%s/graal-sdk-%s.jar"
+            , graalVmHome, version, version
+        );
+        final var jarCompiler = String.format(
+            "%s/compiler/compiler/%s/compiler-%s.jar"
+            , graalVmHome, version, version
+        );
+
+        var classpath = String.format(
+            "%s:%s:%s:%s"
+            , jarSvm
+            , jarPointsTo
+            , jarSdk
+            , jarCompiler
+        );
+
+//        var imageClasspath = String.format(
+//            "%s"
+//            , jarSvm
+//        );
+
+        var command = Arrays.asList(
+            "java"
+            , "-cp"
+            , classpath
+            , "com.oracle.svm.hosted.NativeImageGeneratorRunner"
+//            , "-imagecp"
+//            , imageClasspath
+        );
+
+        System.out.println(String.join(" ", command));
+
         var outputDir = Path.of(".");
         var errorReportLatch = new CountDownLatch(1);
 
