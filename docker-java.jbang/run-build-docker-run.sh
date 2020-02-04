8e87ac3567382840ef759fc31aab2d9f45e92e91
Galder Zamarreño <galder@zamarreno.com>
2020-02-04 14:37:40 +0100

    Just use java as a way to construct the command to run

---
+++ docker-java.jbang/run-build-docker-run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+eval $(jbang src/build_docker_run.java)
---
+++ docker-java.jbang/src/build_docker_run.java
@@ -0,0 +1,7 @@
+public class build_docker_run
+{
+    public static void main(String[] args)
+    {
+        System.out.println("docker run -it --entrypoint /bin/sh mandrel-productization-builder");
+    }
+}
