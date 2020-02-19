8a2d8df659cfeb192534810f31756c0b1c3c50ae
Galder Zamarreño <galder@zamarreno.com>
2020-02-19 16:41:00 +0100

    Playing with Path

---
+++ sandbox/src/main/java/j/files/PathsPaths.java
@@ -0,0 +1,14 @@
+package j.files;
+
+import java.nio.file.Path;
+
+public class PathsPaths
+{
+    public static void main(String[] args)
+    {
+        final var path = Path.of("home", "g", "mandrel-mandrel");
+        System.out.println(path);
+        final var last = path.getFileName().toString();
+        System.out.println(last);
+    }
+}
