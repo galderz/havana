5cc7333ea5e20cb38d34fdb082e0ac36e157ed37
Galder Zamarreño <galder@zamarreno.com>
2020-02-25 10:25:27 +0100

    Backup or restore

---
+++ sandbox/src/main/java/j/files/BackupOrRestore.java
@@ -0,0 +1,32 @@
+package j.files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class BackupOrRestore
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            final var base = System.getProperty("base");
+            Path backupMxPy = Path.of(base, "mx.py.backup");
+            Path mxPy = Path.of(base, "mx.py");
+            if (!backupMxPy.toFile().exists())
+            {
+                Files.copy(mxPy, backupMxPy);
+            }
+            else
+            {
+                Files.copy(backupMxPy, mxPy, StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
