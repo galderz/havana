51cce9c0e269f5246fc07490693246e3b03a1c3d
Galder Zamarreño <galder@zamarreno.com>
2020-03-13 07:43:00 +0100

    Resolve sibling

--- sandbox/src/main/java/jawa/files/PathsPaths.java
+++ sandbox/src/main/java/jawa/files/PathsPaths.java
@@ -10,5 +10,12 @@ public class PathsPaths
         System.out.println(path);
         final var last = path.getFileName().toString();
         System.out.println(last);
+
+        final var resources = Path.of("/Users", "blah", "1", "mandrel-packaging", "resources");
+        System.out.println(resources);
+        final var underResources = resources.resolve("blah-blah");
+        System.out.println(underResources);
+        final var siblingResources = resources.resolveSibling("target");
+        System.out.println(siblingResources);
     }
 }
