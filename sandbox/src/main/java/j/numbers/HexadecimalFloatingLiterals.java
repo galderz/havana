8d330334ee85847b77bca9be4eaf99bd3ff48cca
Galder Zamarreño <galder@zamarreno.com>
2020-03-18 10:05:34 +0100

    Hexadecimal floating literals

---
+++ sandbox/src/main/java/j/numbers/HexadecimalFloatingLiterals.java
@@ -0,0 +1,15 @@
+package j.numbers;
+
+public class HexadecimalFloatingLiterals
+{
+    public static void main(String[] args)
+    {
+        double a = 0x1.8p1;
+        System.out.println(a);
+        System.out.printf("%a%n", a);
+
+        double b = 9.33263618503218878990e-302;
+        System.out.printf("%a%n", b);
+        System.out.println(b);
+    }
+}
