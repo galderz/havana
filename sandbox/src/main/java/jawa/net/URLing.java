6bb05c09b65d8e973b33b8e0d5ca54cfc8b49950
Galder Zamarreño <galder@zamarreno.com>
2020-03-20 11:36:56 +0100

    URIs offer more flexibility to navigate in the path

--- sandbox/src/main/java/jawa/net/URLing.java
+++ sandbox/src/main/java/jawa/net/URLing.java
@@ -1,19 +1,22 @@
 package jawa.net;
 
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
 
 public class URLing
 {
-    public static void main(String[] args) throws MalformedURLException
+    public static void main(String[] args) throws MalformedURLException, URISyntaxException
     {
         var urlText = "https://github.com/quarkusio/quarkus/tree/master";
         var url = new URL(urlText);
-        System.out.println(url.getPath());
 
+        System.out.println("Option 1:");
+        System.out.println(url);
         final var elements = url.getPath().split("/");
         System.out.println(elements[elements.length - 1]);
-
         final var repo = String.format(
             "%s://%s/%s/%s"
             , url.getProtocol()
@@ -22,5 +25,11 @@ public class URLing
             , elements[2]
         );
         System.out.println(repo);
+
+        System.out.println("\nOption 2:");
+        final var uri = url.toURI();
+        System.out.println(uri);
+        System.out.println(Path.of(uri.getPath()).getFileName().toString());
+        System.out.println(uri.resolve(".."));
     }
 }
