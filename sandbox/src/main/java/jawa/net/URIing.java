c804abed9eb6c61c08afa74d609bf8585f6c524b
Galder Zamarreño <galder@zamarreno.com>
2020-04-07 11:51:03 +0200

    Shortened URIs

--- sandbox/src/main/java/jawa/net/URIing.java
+++ sandbox/src/main/java/jawa/net/URIing.java
@@ -7,7 +7,21 @@ public class URIing
 {
     public static void main(String[] args)
     {
+        shortenedURIs();
         enhancingURIs();
+    }
+
+    private static void shortenedURIs()
+    {
+        final var basic = URI.create(
+            "github://quarkusio/quarkus/master"
+        );
+        System.out.println(basic);
+
+        final var enhanced = URI.create(
+            "github://quarkusio/quarkus/master/galderz/94a22d7"
+        );
+        System.out.println(enhanced);
     }
 
     private static void enhancingURIs()
