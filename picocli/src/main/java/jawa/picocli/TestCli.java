4994e36e09c098c91d5c1e5a7a17c03f0231e3d4
Galder Zamarreño <galder@zamarreno.com>
2020-04-21 10:20:44 +0200

    Avoid using commands that contain `|`
    * `|` character is used for piping on unix.
    * The shell will split the command at that point.
    * Instead use a normal list and split things manually.
    * Also, force running with assertions,
    and use them to validate the results are the expected ones.

--- picocli/src/main/java/jawa/picocli/TestCli.java
+++ picocli/src/main/java/jawa/picocli/TestCli.java
@@ -3,14 +3,33 @@ package jawa.picocli;
 import picocli.CommandLine;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TestCli
 {
     public static void main(String[] ignore)
     {
+        Assert.check();
+
+        {
+            System.out.println("> Test multiple with added test parameters");
+            String[] args = {
+                "--additional-test-args"
+                , "quarkus=-rf,:quarkus-elytron-security-ldap-integration-test"
+                , "--additional-test-args"
+                , "quarkus-platform=-Dcamel-quarkus.version=1.1.0-SNAPSHOT,-Dquarkus.version=999-SNAPSHOT"
+                , "--also-test"
+                , "https://github.com/quarkusio/quarkus-platform/tree/master"
+            };
+            final var test = new Test();
+            new CommandLine(test).parseArgs(args);
+            assert test.additionalTestArgs.size() == 2;
+            assert test.additionalTestArgs.get(0).equals("quarkus=-rf,:quarkus-elytron-security-ldap-integration-test");
+            assert test.additionalTestArgs.get(1).equals("quarkus-platform=-Dcamel-quarkus.version=1.1.0-SNAPSHOT,-Dquarkus.version=999-SNAPSHOT");
+            assert test.alsoTest.size() == 1;
+            assert test.alsoTest.get(0).equals("https://github.com/quarkusio/quarkus-platform/tree/master");
+        }
+
         {
             System.out.println("> Test only quarkus-platform with specific test parameters");
             String[] args = {
@@ -83,8 +102,8 @@ public class TestCli
         @CommandLine.Option(names = "--suites", split = ",")
         private final List<String> suites = new ArrayList<>();
 
-        @CommandLine.Option(names = "--additional-test-args", split = "\\|")
-        private final Map<String, String> additionalTestArgs = new HashMap<>();
+        @CommandLine.Option(names = "--additional-test-args")
+        private final List<String> additionalTestArgs = new ArrayList<>();
 
         @CommandLine.Option(names = "--also-test", split = ",")
         private final List<String> alsoTest = new ArrayList<>();
