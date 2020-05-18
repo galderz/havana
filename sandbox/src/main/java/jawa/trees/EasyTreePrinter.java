07987aeab0744d91e82df3ea9ffe1c6f13994226
Galder Zamarreño <galder@zamarreno.com>
2020-05-18 18:18:31 +0200

    Easy tree printer

---
+++ sandbox/src/main/java/jawa/trees/EasyTreePrinter.java
@@ -0,0 +1,49 @@
+package jawa.trees;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class EasyTreePrinter
+{
+    public static void main(String[] args)
+    {
+        final var b = new TreeNode("B", new ArrayList<>());
+        final var c = new TreeNode("C", new ArrayList<>());
+        final var root = new TreeNode("A", Arrays.asList(b, c));
+        // b.children.add(root); does not handle cycles
+        System.out.println(root);
+    }
+
+    public static class TreeNode {
+
+        final String name;
+        final List<TreeNode> children;
+
+        public TreeNode(String name, List<TreeNode> children) {
+            this.name = name;
+            this.children = children;
+        }
+
+        public String toString() {
+            StringBuilder buffer = new StringBuilder(50);
+            print(buffer, "", "");
+            return buffer.toString();
+        }
+
+        private void print(StringBuilder buffer, String prefix, String childrenPrefix) {
+            buffer.append(prefix);
+            buffer.append(name);
+            buffer.append('\n');
+            for (Iterator<TreeNode> it = children.iterator(); it.hasNext();) {
+                TreeNode next = it.next();
+                if (it.hasNext()) {
+                    next.print(buffer, childrenPrefix + "├── ", childrenPrefix + "│   ");
+                } else {
+                    next.print(buffer, childrenPrefix + "└── ", childrenPrefix + "    ");
+                }
+            }
+        }
+    }
+}
