9c8170656dcd95522c3d50e87eaab52bff2d92e7
Galder Zamarreño <galder@zamarreno.com>
2020-06-29 16:46:47 +0200

    Jar to source jar example

---
+++ io/src/main/java/io/JarToSourceJar.java
@@ -0,0 +1,24 @@
+package io;
+
+import java.nio.file.Path;
+
+public class IoFiles
+{
+    public static void main(String[] args)
+    {
+        System.out.println(
+            toSourceJar(
+                Path.of("/Users/g/.m2/repository/jakarta/annotation/jakarta.annotation-api/1.3.5/jakarta.annotation-api-1.3.5.jar")
+            )
+        );
+    }
+
+    static Path toSourceJar(Path path)
+    {
+        final var parent = path.getParent();
+        final var fileName = path.getFileName().toString();
+        final var extensionIndex = fileName.lastIndexOf('.');
+        final var sourcesFileName = String.format("%s-sources.jar", fileName.substring(0, extensionIndex));
+        return parent.resolve(sourcesFileName);
+    }
+}
