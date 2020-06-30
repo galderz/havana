3d1417a656d825e3b74908c86a4db4b7983cabdf
Galder Zamarreño <galder@zamarreno.com>
2020-06-30 19:36:42 +0200

    Add an old third party dependency to the source cache example

--- substratevm/sourcecache/sourcecache-client/src/main/java/svm/sourcecache/client/Main.java
+++ substratevm/sourcecache/sourcecache-client/src/main/java/svm/sourcecache/client/Main.java
@@ -1,5 +1,6 @@
 package svm.sourcecache.client;
 
+import org.jboss.slf4j.JBossLoggerFactory;
 import svm.sourcecache.library.Greeter;
 
 public class Main
@@ -7,5 +8,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println(Greeter.greet(args[0]));
+        final var hashCode = System.identityHashCode(JBossLoggerFactory.class);
+        System.out.println("A value: " + hashCode / 41);
     }
 }
