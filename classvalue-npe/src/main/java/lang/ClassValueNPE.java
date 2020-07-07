922215e996e681cf373acbc12380ec2b96837349
Galder Zamarreño <galder@zamarreno.com>
2020-07-07 17:47:11 +0200

    More tries to reproduce NPE

--- classvalue-npe/src/main/java/lang/ClassValueNPE.java
+++ classvalue-npe/src/main/java/lang/ClassValueNPE.java
@@ -1,14 +1,86 @@
 package lang;
 
 import java.lang.invoke.MethodHandles;
+import java.util.concurrent.TimeUnit;
 
 public class ClassValueNPE
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        final var g1 = MethodHandles.arrayElementGetter(Object[].class);
-        System.out.println(g1);
-        final var g2 = MethodHandles.arrayElementGetter(Object[].class);
-        System.out.println(g2);
+        final var objectArrayGetter = new ObjectArrayGetter();
+        objectArrayGetter.start();
+        objectArrayGetter.join();
+
+        final var slowIntArrayGetter = new SlowIntArrayGetter();
+        slowIntArrayGetter.start();
+
+        final var fastIntArrayGetter = new FastIntArrayGetter();
+        fastIntArrayGetter.start();
+
+        fastIntArrayGetter.join();
+        slowIntArrayGetter.join();
+    }
+
+    static final class FastIntArrayGetter extends Thread
+    {
+        public FastIntArrayGetter()
+        {
+            super("fast-thread");
+        }
+
+        @Override
+        public void run()
+        {
+            ClassValueNPE.sleepShort();
+            log(MethodHandles.arrayElementGetter(int[].class));
+        }
+    }
+
+    static final class SlowIntArrayGetter extends Thread
+    {
+        public SlowIntArrayGetter()
+        {
+            super("slow-thread");
+        }
+
+        @Override
+        public void run()
+        {
+            log(MethodHandles.arrayElementGetter(int[].class));
+        }
+    }
+
+    static final class ObjectArrayGetter extends Thread
+    {
+        public ObjectArrayGetter()
+        {
+            super("object-thread");
+        }
+
+        @Override
+        public void run()
+        {
+            ClassValueNPE.sleepShort();
+            log(MethodHandles.arrayElementGetter(Object[].class));
+        }
+    }
+
+    static void log(Object... args)
+    {
+        final var threadName = Thread.currentThread().getName();
+        final var msg = String.format("%s", args);
+        System.out.printf("[%s] %s%n", threadName, msg);
+    }
+
+    static void sleepShort()
+    {
+        try
+        {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }
