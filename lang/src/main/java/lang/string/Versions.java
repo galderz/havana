c5e764b2e6ab15103bd43992ca1ef47e0134be8a
Galder Zamarreño <galder@zamarreno.com>
2020-08-05 11:58:10 +0200

    Detect distribution

--- lang/src/main/java/lang/string/Versions.java
+++ lang/src/main/java/lang/string/Versions.java
@@ -2,7 +2,6 @@ package lang.string;
 
 import lang.Assert;
 
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
@@ -12,15 +11,15 @@ public class Versions
     {
         Assert.check();
 
-        final var V_SNAPSHOT = check(Integer.MAX_VALUE, Integer.MAX_VALUE).compose(Version::of);
-        final var V_1_0 = check(1, 0).compose(Version::of);
-        final var V_19_0 = check(19, 0).compose(Version::of);
-        final var V_20_0 = check(20, 0).compose(Version::of);
-        final var V_20_1 = check(20, 1).compose(Version::of);
-        final var V_21_0 = check(21, 0).compose(Version::of);
+        final var V_SNAPSHOT = check(Integer.MAX_VALUE, Integer.MAX_VALUE, Version.Distribution.MANDREL).compose(Version::of);
+        final var V_1_0 = check(1, 0, Version.Distribution.ORACLE).compose(Version::of);
+        final var V_19_0 = check(19, 0, Version.Distribution.ORACLE).compose(Version::of);
+        final var V_20_0 = check(20, 0, Version.Distribution.ORACLE).compose(Version::of);
+        final var V_20_1 = check(20, 1, Version.Distribution.ORACLE).compose(Version::of);
+        final var V_21_0 = check(21, 0, Version.Distribution.ORACLE).compose(Version::of);
         {
             final var version = V_SNAPSHOT.apply("GraalVM Version beb2fd6 (Mandrel Distribution) (Java Version 11.0.9-internal)");
-            assert version.distro == Version.Distribution.ORACLE; // TODO fix
+            assert version.distro == Version.Distribution.MANDREL;
         }
         {
             final var version = V_20_0.apply("GraalVM Version 20.0.0");
@@ -54,12 +53,13 @@ public class Versions
         }
     }
 
-    static Function<Version, Version> check(int major, int minor)
+    static Function<Version, Version> check(int major, int minor, Version.Distribution distro)
     {
         return version ->
         {
             assert version.major == major : version;
             assert version.minor == minor : version;
+            assert version.distro == distro : distro;
             return version;
         };
     }
@@ -68,11 +68,12 @@ public class Versions
     {
         private static final Pattern PATTERN = Pattern.compile(
             // "GraalVM Version (1.0.0|([1-9][0-9]).([0-3]).[0-9]|\\p{XDigit}{7})\\s*(\\(Mandrel Distribution\\))?\\s*"
-            "GraalVM Version ((1|[1-9][0-9]).([0-2]).[0-9]|\\p{XDigit}{7})\\s*"
+            "GraalVM Version ((1|[1-9][0-9]).([0-2]).[0-9]|\\p{XDigit}{7})\\s*(\\(Mandrel Distribution\\))?\\s*"
         );
 
-        static final Version UNVERSIONED = new Version(-1, -1, Distribution.ORACLE);
-        static final Version SNAPSHOT = new Version(Integer.MAX_VALUE, Integer.MAX_VALUE, Distribution.ORACLE);
+        static final Version UNVERSIONED = new Version(-1, -1, null);
+        static final Version SNAPSHOT_ORACLE = new Version(Integer.MAX_VALUE, Integer.MAX_VALUE, Distribution.ORACLE);
+        static final Version SNAPSHOT_MANDREL = new Version(Integer.MAX_VALUE, Integer.MAX_VALUE, Distribution.MANDREL);
 
         final int major;
         final int minor;
@@ -107,10 +108,10 @@ public class Versions
             final var matcher = PATTERN.matcher(version);
             if (matcher.find() && matcher.groupCount() >= 3)
             {
-                final var first = matcher.group(1);
-                if (first.length() == 7)
+                if (isSnapshot(matcher.group(1)))
                 {
-                    return SNAPSHOT;
+                    final var distro = matcher.group(4);
+                    return isMandrel(distro) ? SNAPSHOT_MANDREL : SNAPSHOT_ORACLE;
                 }
                 else
                 {
@@ -122,6 +123,16 @@ public class Versions
             }
 
             return UNVERSIONED;
+        }
+
+        private static boolean isSnapshot(String s)
+        {
+            return s.length() == 7;
+        }
+
+        private static boolean isMandrel(String s)
+        {
+            return "(Mandrel Distribution)".equals(s);
         }
 
         @Override
