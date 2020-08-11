4e3c063e12edb541b8d35f7158fc31dc1ada5e7d
Galder Zamarreño <galder@zamarreno.com>
2020-08-11 12:39:01 +0200

    Extend with a test that sets last value to volatile

--- classvalue-npe-jcstress/src/main/java/lang/JMMSample_06_Finals.java
+++ classvalue-npe-jcstress/src/main/java/lang/JMMSample_06_Finals.java
@@ -269,4 +269,65 @@ public class JMMSample_06_Finals
             }
         }
     }
+
+    @JCStressTest
+    @Outcome(id = "-1", expect = ACCEPTABLE, desc = "Object is not seen yet.")
+    @Outcome(id = "8", expect = ACCEPTABLE, desc = "Seen the complete object.")
+    @Outcome(expect = FORBIDDEN, desc = "Seeing partially constructed object.")
+    @State
+    public static class FinalInitExtendedVolatile
+    {
+        int v = 1;
+
+        MyObjectExtended o;
+
+        @Actor
+        public void actor1()
+        {
+            o = new MyObjectExtended(v);
+        }
+
+        @Actor
+        public void actor2(I_Result r)
+        {
+            MyObjectExtended o = this.o;
+            if (o != null)
+            {
+                r.r1 = o.x8 + o.x7 + o.x6 + o.x5 + o.x4 + o.x3 + o.x2 + o.x1;
+            }
+            else
+            {
+                r.r1 = -1;
+            }
+        }
+
+        public static class MyObject
+        {
+            final int x4;
+
+            public MyObject(int v)
+            {
+                x4 = v;
+            }
+        }
+
+        public static class MyObjectExtended extends MyObject
+        {
+            int x1, x2, x3;
+            int x5, x6, x7;
+            volatile int x8;
+
+            public MyObjectExtended(int v)
+            {
+                super(v);
+                x1 = v;
+                x2 = v;
+                x3 = v;
+                x5 = v;
+                x6 = v;
+                x7 = v;
+                x8 = v;
+            }
+        }
+    }
 }
