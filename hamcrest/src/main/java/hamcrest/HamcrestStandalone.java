c9280666e95f0302a022db128e1a2d8086b10d31
Galder Zamarreño <galder@zamarreno.com>
2020-08-24 22:07:11 +0200

    Make hamcrest example independent

---
+++ hamcrest/src/main/java/hamcrest/HamcrestStandalone.java
@@ -0,0 +1,14 @@
+package hamcrest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestStandalone
+{
+    public static void main(String[] args)
+    {
+        String theBiscuit = "Ginger";
+        String myBiscuit = "Ginger";
+        assertThat(theBiscuit, equalTo(myBiscuit));
+    }
+}
