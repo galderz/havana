ecc334a6ab3c7d45588c6508adac5ac94a159b36
Galder Zamarreño <galder@zamarreno.com>
2020-09-04 16:12:28 +0200

    JUnit programmatic example

---
+++ junit-programmatic/src/main/java/junit/SecondUnitTest.java
@@ -0,0 +1,20 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SecondUnitTest
+{
+    @Test
+    public void whenSomething_thenSomething()
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whensomethingElse_thenSomethingElse()
+    {
+        assertTrue(true);
+    }
+}
