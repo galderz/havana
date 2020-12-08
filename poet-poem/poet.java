8c9da9649ceb6ba8a96500909e7e15f051a231e0
Galder Zamarreño <galder@redhat.com>
2020-12-08 15:25:06 +0100

    A clumsy poet in action

--- poet-poem/poet.java
+++ poet-poem/poet.java
@@ -1,10 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.5.2
 //DEPS com.squareup:javapoet:1.13.0
+//DEPS org.jooq:joor:0.9.13
 
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
+import org.joor.Reflect;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
@@ -37,18 +39,28 @@ class poet implements Callable<Integer>
             .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
             .returns(void.class)
             .addParameter(String[].class, "args")
-            .addStatement("$T.out.println($S)", System.class, "Hello, " + greeting)
+            .addStatement("$T.xxx.println($S)", System.class, "Hello, " + greeting)
             .build();
 
-        TypeSpec poem = TypeSpec.classBuilder("poem")
+        TypeSpec poemType = TypeSpec.classBuilder("poem")
             .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
             .addMethod(main)
             .build();
 
-        JavaFile javaFile = JavaFile.builder("", poem)
+        JavaFile javaFile = JavaFile.builder("", poemType)
             .build();
 
-        javaFile.writeTo(Path.of("."));
+        try
+        {
+            Reflect.compile(
+                "poem",
+                javaFile.toString()).create().get();
+        }
+        finally
+        {
+            javaFile.writeTo(Path.of("."));
+        }
+
         return 0;
     }
 }
