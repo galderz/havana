f3747ca2d13d48ac536d2e1c693ac53aeea3b8b7
Galder Zamarreño <galder@redhat.com>
2020-12-08 13:09:03 +0100

    JBang using javapoet to generate the most basic JBang hello world

---
+++ poet-poem/poet.java
@@ -0,0 +1,54 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS info.picocli:picocli:4.5.2
+//DEPS com.squareup:javapoet:1.13.0
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import javax.lang.model.element.Modifier;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+@Command(
+    name = "poet"
+    , mixinStandardHelpOptions = true
+    , version = "poet 0.1"
+    , description = "poet made with jbang"
+)
+class poet implements Callable<Integer>
+{
+    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
+    private String greeting;
+
+    public static void main(String... args)
+    {
+        int exitCode = new CommandLine(new poet()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception
+    {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+            .returns(void.class)
+            .addParameter(String[].class, "args")
+            .addStatement("$T.out.println($S)", System.class, "Hello, " + greeting)
+            .build();
+
+        TypeSpec poem = TypeSpec.classBuilder("poem")
+            .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+            .addMethod(main)
+            .build();
+
+        JavaFile javaFile = JavaFile.builder("", poem)
+            .build();
+
+        javaFile.writeTo(Path.of("."));
+        return 0;
+    }
+}
