03b8d647d36b77562da73dec53b3817088524b6d
Galder Zamarreño <galder@redhat.com>
2021-01-19 18:36:01 +0100

    Calculate max signed/unsigned values given number of bits

---
+++ lang/src/main/java/lang/system/MinMax.java
@@ -0,0 +1,21 @@
+package lang.system;
+
+public class MinMax
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("signed Integer.MAX_VALUE = %s | %s%n", Integer.MAX_VALUE, signedMaxValue(32));
+        System.out.printf("signed Long.MAX_VALUE = %s | %s%n", Long.MAX_VALUE, signedMaxValue(64));
+        System.out.printf("unsigned integer max = %s%n", Long.toUnsignedString(unsignedMaxValue(32)));
+        System.out.printf("unsigned long max = %s%n", Long.toUnsignedString(unsignedMaxValue(64)));
+    }
+
+    static long unsignedMaxValue(int minBits) {
+        final var numBits = minBits - 1;
+        return 1L << numBits | (1L << numBits) - 1;
+    }
+
+    static long signedMaxValue(int minBits) {
+        return (1L << (minBits - 1)) - 1;
+    }
+}
