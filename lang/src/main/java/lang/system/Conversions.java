60343aefb9bb767834251a340d69bec9684743e6
Galder Zamarreño <galder@redhat.com>
2021-01-19 08:33:18 +0100

    Try widening conversions with max/min values

--- lang/src/main/java/lang/system/Conversions.java
+++ lang/src/main/java/lang/system/Conversions.java
@@ -4,7 +4,7 @@ import static java.lang.System.out;
 
 public class Conversions
 {
-    public static void main(String[] args)
+    public static int main()
     {
         {
             putchar('b');
@@ -15,6 +15,20 @@ public class Conversions
 
             byte low = Byte.MIN_VALUE + 1;
             byte high = Byte.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (byte)((short) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((short) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((int) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((int) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((double) high) == 0 ? '.' : 'F');
+            low = Byte.MIN_VALUE;
+            high = Byte.MAX_VALUE;
             putchar(low - low == 0 ? '.' : 'F');
             putchar(high - high == 0 ? '.' : 'F');
             putchar(low - (byte)((short) low) == 0 ? '.' : 'F');
@@ -50,6 +64,18 @@ public class Conversions
             putchar(high - (short)((float) high) == 0 ? '.' : 'F');
             putchar(low - (short)((double) low) == 0 ? '.' : 'F');
             putchar(high - (short)((double) high) == 0 ? '.' : 'F');
+            low = Short.MIN_VALUE;
+            high = Short.MAX_VALUE;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (short)((int) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((int) high) == 0 ? '.' : 'F');
+            putchar(low - (short)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (short)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (short)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((double) high) == 0 ? '.' : 'F');
             putchar('\n');
         }
 
@@ -62,6 +88,18 @@ public class Conversions
 
             char low = Character.MIN_VALUE + 1;
             char high = Character.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (char)((int) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((int) high) == 0 ? '.' : 'F');
+            putchar(low - (char)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (char)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (char)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((double) high) == 0 ? '.' : 'F');
+            low = Character.MIN_VALUE;
+            high = Character.MAX_VALUE;
             putchar(low - low == 0 ? '.' : 'F');
             putchar(high - high == 0 ? '.' : 'F');
             putchar(low - (char)((int) low) == 0 ? '.' : 'F');
@@ -92,6 +130,16 @@ public class Conversions
             putchar(high - (int)((float) high) == -1 ? '.' : 'F'); // loss of precision
             putchar(low - (int)((double) low) == 0 ? '.' : 'F');
             putchar(high - (int)((double) high) == 0 ? '.' : 'F');
+            low = Integer.MIN_VALUE;
+            high = Integer.MAX_VALUE;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (int)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (int)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (int)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (int)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (int)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (int)((double) high) == 0 ? '.' : 'F');
             putchar('\n');
         }
 
@@ -110,8 +158,23 @@ public class Conversions
             putchar(high - (long)((float) high) == -1 ? '.' : 'F'); // loss of precision
             putchar(low - (long)((double) low) == 1 ? '.' : 'F'); // loss of precision
             putchar(high - (long)((double) high) == -1 ? '.' : 'F'); // loss of precision
+            low = Long.MIN_VALUE;
+            high = Long.MAX_VALUE;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (long)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (long)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (long)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (long)((double) high) == 0 ? '.' : 'F');
             putchar('\n');
         }
+
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        main();
     }
 
     private static void putchar(char c)
