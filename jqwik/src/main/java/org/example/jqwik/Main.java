3d59a5775f3c62bf4603592e993d0a68c4548333
Galder Zamarreño <galder@redhat.com>
2021-02-25 12:24:09 +0100

    jqwik 1.4.0 broken, so stick to 1.3.10

--- jqwik/src/main/java/org/example/jqwik/Main.java
+++ jqwik/src/main/java/org/example/jqwik/Main.java
@@ -1,6 +1,14 @@
 package org.example.jqwik;
 
 import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.Combinators;
+import net.jqwik.api.Shrinkable;
+
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main
 {
@@ -20,6 +28,38 @@ public class Main
         {
             final var entries = Arbitraries.entries(Arbitraries.longs(), Arbitraries.longs());
             System.out.println(entries.sampleStream().limit(1000).count());
+        }
+
+        {
+            System.out.println(
+                Arbitraries.doubles().sampleStream().limit(1000).filter(d -> Double.MIN_VALUE == d).findFirst()
+            );
+
+            System.out.println(
+                Arbitraries.doubles()
+                    .edgeCases(cfg -> cfg.add(Double.MIN_VALUE))
+                    .sampleStream().limit(1000)
+                    .filter(d -> Double.MIN_VALUE == d).findFirst()
+            );
+
+            // Only works in 1.3.10
+            System.out.println(
+                Arbitraries.doubles().edgeCases(cfg -> cfg.add(Double.MIN_VALUE))
+                    .edgeCases().suppliers().stream().map(Supplier::get).map(Shrinkable::value)
+                    //.collect(Collectors.toList())
+                    .filter(d -> d == Double.MIN_VALUE).findFirst()
+            );
+
+            final var combinator = Combinators.combine(
+                Arbitraries.doubles().edgeCases(cfg -> cfg.add(Double.MIN_VALUE))
+                , Arbitraries.doubles().edgeCases(cfg -> cfg.add(Double.MIN_VALUE))
+            );
+
+            System.out.println(
+                combinator.as(Map::entry)
+                    .edgeCases().suppliers().stream().map(Supplier::get).map(Shrinkable::value)
+                    .collect(Collectors.toList())
+            );
         }
     }
 }
