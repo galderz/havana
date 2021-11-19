484ad4e14ca29f59112df4239dd945eccac4744e
Galder Zamarreño <galder@zamarreno.com>
2021-11-19 14:23:05 +0100

    Add doubles examples

---
+++ lang/src/main/java/lang/arithmetics/Doubles.java
@@ -0,0 +1,13 @@
+package lang.arithmetics;
+
+public class Doubles
+{
+    public static void main(String[] args)
+    {
+        System.out.println((60.0 / 9.81) * 3.711 == 22.69724770642202);
+        System.out.println((60.0 / 9.81) * 24.79 == 151.62079510703364);
+
+        System.out.println(20 * 365 / 88);
+        System.out.println(Math.round(60 / 29.5));
+    }
+}
