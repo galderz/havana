c0858d072e21022cb7fad0a27039372744288884
Galder Zamarreño <galder@zamarreno.com>
2022-03-08 12:21:43 +0100

    Parsing duration very basically

---
+++ time/src/main/java/time/ParseDuration.java
@@ -0,0 +1,22 @@
+package time;
+
+import java.time.Duration;
+
+public class ParseDuration
+{
+    public static void main(String[] args)
+    {
+        System.out.println(
+            parseDuration("0:42.90")
+        );
+    }
+
+    public static Duration parseDuration(String duration)
+    {
+        return Duration.parse(
+            "PT"
+            + duration.replace(':', 'M')
+            + "S"
+        );
+    }
+}
