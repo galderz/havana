aa739d52abc7b302adfe7dbb1e6d4fbfcebdf03c
Galder Zamarreño <galder@zamarreno.com>
2022-05-27 14:07:05 +0200

    Add assert class

---
+++ util/src/main/java/util/Assert.java
@@ -0,0 +1,16 @@
+package util;
+
+public final class Assert
+{
+    public static void check()
+    {
+        boolean enabled = false;
+        assert enabled = true;
+        if (!enabled)
+            throw new AssertionError("assert not enabled");
+    }
+
+    private Assert()
+    {
+    }
+}
