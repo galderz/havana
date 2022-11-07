acc68bfb32e0086a4cf0499cc256a11edac35ebb
Galder Zamarreño <galder@zamarreno.com>
2022-11-07 07:47:26 +0100

    Avoid allocation the sample object, instead pre-allocate a 2d array

--- util/src/main/java/util/SamplePriorityQueue.java
+++ util/src/main/java/util/SamplePriorityQueue.java
@@ -4,13 +4,17 @@ import java.util.Objects;
 
 public class SamplePriorityQueue
 {
-    private final Sample[] items;
+    private final Object[][] items;
     private int count;
     private long total;
 
     public SamplePriorityQueue(int size)
     {
-        this.items = new Sample[size];
+        this.items = new Object[size][];
+        for (int i = 0; i < this.items.length; i++)
+        {
+            this.items[i] = new Object[2];
+        }
     }
 
     /**
@@ -19,15 +23,14 @@ public class SamplePriorityQueue
      * This method does not check if the queue has enough capacity.
      * It's up to the caller decide how to deal with a full queue.
      */
-    public void push(Sample sample)
+    public void push(long span)
     {
-        assert sample != null;
-        assert items[count] == null;
+        assert span(items[count]) == null;
 
-        items[count] = sample;
+        setSpan(span, items[count]);
         count++;
         moveUp(count - 1);
-        total += sample.span;
+        total += span;
     }
 
     /**
@@ -36,21 +39,21 @@ public class SamplePriorityQueue
      *
      * @return a Sample or null if empty
      */
-    public Sample poll()
+    public Object[] poll()
     {
         if (count == 0)
         {
             return null;
         }
 
-        final Sample head = items[0];
+        final Object[] head = items[0];
         assert head != null;
         swap(0, count - 1);
         count--;
         assert head == items[count];
         items[count] = null;
         moveDown(0);
-        total -= head.span;
+        total -= span(head);
         return head;
     }
 
@@ -65,10 +68,10 @@ public class SamplePriorityQueue
         {
             int j = -1;
             int r = right(i);
-            if (r < count && items[r].span < items[i].span)
+            if (r < count && span(items[r]) < span(items[i]))
             {
                 int l = left(i);
-                if (items[l].span < items[r].span)
+                if (span(items[l]) < span(items[r]))
                 {
                     j = l;
                 }
@@ -80,7 +83,7 @@ public class SamplePriorityQueue
             else
             {
                 int l = left(i);
-                if (l < count && items[l].span < items[i].span)
+                if (l < count && span(items[l]) < span(items[i]))
                 {
                     j = l;
                 }
@@ -105,7 +108,7 @@ public class SamplePriorityQueue
     private void moveUp(int i)
     {
         int parent = parent(i);
-        while (i > 0 && items[i].span < items[parent].span)
+        while (i > 0 && span(items[i]) < span(items[parent]))
         {
             swap(i, parent);
             i = parent;
@@ -115,16 +118,26 @@ public class SamplePriorityQueue
 
     private void swap(int i, int j)
     {
-        final Sample tmp = items[i];
+        final Object[] tmp = items[i];
         items[i] = items[j];
         items[j] = tmp;
-        items[i].index = i;
-        items[j].index = j;
+        // items[i].index = i;
+        // items[j].index = j;
     }
 
     private static int parent(int i)
     {
         return (i - 1) / 2;
+    }
+
+    private static void setSpan(long span, Object[] sample)
+    {
+        sample[0] = span;
+    }
+
+    static Long span(Object[] sample)
+    {
+        return (Long) sample[0];
     }
 
     public static void main(String[] args)
@@ -137,41 +150,28 @@ public class SamplePriorityQueue
     private static void testOfferThenPoll()
     {
         SamplePriorityQueue queue = new SamplePriorityQueue(10);
-        queue.push(new Sample().setSpan(200));
-        queue.push(new Sample().setSpan(400));
-        queue.push(new Sample().setSpan(300));
-        queue.push(new Sample().setSpan(500));
-        queue.push(new Sample().setSpan(100));
+        queue.push(200);
+        queue.push(400);
+        queue.push(300);
+        queue.push(500);
+        queue.push(100);
 
-        assert 100 == queue.poll().span;
-        assert 200 == queue.poll().span;
-        assert 300 == queue.poll().span;
-        assert 400 == queue.poll().span;
-        assert 500 == queue.poll().span;
+        assert 100 == span(queue.poll());
+        assert 200 == span(queue.poll());
+        assert 300 == span(queue.poll());
+        assert 400 == span(queue.poll());
+        assert 500 == span(queue.poll());
         assert Objects.isNull(queue.poll());
     }
 
     private static void testIsFull()
     {
         SamplePriorityQueue queue = new SamplePriorityQueue(3);
-        queue.push(new Sample().setSpan(300));
+        queue.push(300);
         assert !queue.isFull();
-        queue.push(new Sample().setSpan(200));
+        queue.push(200);
         assert !queue.isFull();
-        queue.push(new Sample().setSpan(100));
+        queue.push(100);
         assert queue.isFull();
     }
-
-    private static final class Sample
-    {
-        int index;
-        long span;
-
-        public Sample setSpan(long span)
-        {
-            this.span = span;
-            return this;
-        }
-    }
 }
-
