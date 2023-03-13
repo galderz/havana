d34f818e723c7ae86b308baa62aaa5f5b0e00d58
Galder Zamarreño <galder@zamarreno.com>
2023-03-13 11:24:53 +0100

    Extract and generalize collection paths

--- util/src/main/java/util/bitmap/v1/PathStoreTest.java
+++ util/src/main/java/util/bitmap/v1/PathStoreTest.java
@@ -3,7 +3,9 @@ package util.bitmap.v1;
 import util.Asserts;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class PathStoreTest
@@ -23,25 +25,38 @@ public class PathStoreTest
         store.addPathLeaf(0, leak);
         store.addPathElement(0, new UnsignedWord("fieldB"), gcRoot, 1);
 
-        List<Path> paths = new ArrayList<>();
+        Map<String, List<Path>> paths = collectPaths(store, List.of(leak));
 
-        final int pathIndex = store.getPathIndex(leak);
-        assert 0 == pathIndex;
-
-        Object next;
-        int elementIndex = 0;
-        while (null != (next = store.getElement(elementIndex, pathIndex)))
-        {
-            final UnsignedWord location = store.getElementLocation(elementIndex, pathIndex);
-            final Path path = new Path(next, location.fieldName);
-            paths.add(path);
-            elementIndex++;
-        }
-
+        assert 1 == paths.size();
         assert List.of(
             new Path("B", "")
             , new Path("A", "fieldB")
-        ).equals(paths);
+        ).equals(paths.get(leak));
+    }
+
+    private static Map<String, List<Path>> collectPaths(PathStore store, List<String> leaks)
+    {
+        final Map<String, List<Path>> result = new HashMap<>();
+        for (String leak : leaks)
+        {
+            List<Path> paths = new ArrayList<>();
+
+            final int pathIndex = store.getPathIndex(leak);
+            assert 0 == pathIndex;
+
+            Object next;
+            int elementIndex = 0;
+            while (null != (next = store.getElement(elementIndex, pathIndex)))
+            {
+                final UnsignedWord location = store.getElementLocation(elementIndex, pathIndex);
+                final Path path = new Path(next, location.fieldName);
+                paths.add(path);
+                elementIndex++;
+            }
+
+            result.put(leak, paths);
+        }
+        return result;
     }
 
     static final class Path
