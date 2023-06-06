884981d22365b9cf762956d2c230f546df77a2f7
Galder Zamarreño <galder@zamarreno.com>
2023-06-06 12:20:05 +0200

    Testing biggest array

--- util/src/main/java/util/bitmap/v1/BitMapTest.java
+++ util/src/main/java/util/bitmap/v1/BitMapTest.java
@@ -2,6 +2,8 @@ package util.bitmap.v1;
 
 import util.Asserts;
 
+import java.util.BitSet;
+
 public class BitMapTest
 {
     public static void main(String[] args)
@@ -10,6 +12,7 @@ public class BitMapTest
         testSign();
         testHighBits();
         testLowBits();
+        // testBiggestArray();
     }
 
     private static void testLowBits()
@@ -32,5 +35,14 @@ public class BitMapTest
         final BitMap bitMap = new BitMap();
         assert bitMap.getSignum(-9223090557539720352L) < 0; // 800100010295e360
         assert bitMap.getSignum(281479315055456L) > 0;      // 000100010295e360
+    }
+
+    private static void testBiggestArray()
+    {
+        final BitSet thirtyBitsBitSet = new BitSet(1 << 30);
+        System.out.println(thirtyBitsBitSet);
+        System.out.println(thirtyBitsBitSet.size());
+        final BitSet thirtyOneBitsBitSet = new BitSet(1 << 31);
+        System.out.println(thirtyOneBitsBitSet);
     }
 }
