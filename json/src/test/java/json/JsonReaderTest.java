864a25430daf87da05a276b178eb2668d8050cc5
Galder Zamarreño <galder@zamarreno.com>
2023-11-09 08:49:27 +0100

    Rename to JsonReaderTest

--- json/src/test/java/json/JsonReaderTest.java
+++ json/src/test/java/json/JsonReaderTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 
-public class JsonTest
+public class JsonReaderTest
 {
     @Test
     public void testResourceJson()
