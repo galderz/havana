a19d647ce09321eb1d8dc17d2405e4833e06f535
Galder Zamarreño <galder@zamarreno.com>
2024-01-09 19:33:18 +0100

    Make the test OOME after C1 has kicked in

--- bugs/array-copy-node/8302850/TestArrayCloneOOME.java
+++ bugs/array-copy-node/8302850/TestArrayCloneOOME.java
@@ -3,9 +3,11 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class TestArrayCloneOOME
 {
+    static int[][] leak;
+
     public static void main(String[] args)
     {
-        final int size = Integer.MAX_VALUE >> 9;
+        final int size = 10_000;
         System.out.println("Size is: " + size);
         final int[] ints = new int[size];
         for (int i = 0; i < ints.length; i++)
@@ -14,10 +16,13 @@ class TestArrayCloneOOME
         }
 
         System.out.println("Clone...");
-        for (int i = 0; i < 1_000; i++)
+        int cloneCount = 1_000;
+        leak = new int[cloneCount][];
+        for (int i = 0; i < cloneCount; i++)
         {
             int[] result = test(ints);
             // System.out.println(Arrays.toString(result));
+            leak[i] = result;
             blackhole(result);
         }
     }
