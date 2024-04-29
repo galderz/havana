567094796394b85332407a53a20533537fbccd19
Galder Zamarreño <galder@zamarreno.com>
2024-04-29 15:59:47 +0200

    Use a simpler test

--- bugs/intrinsify-max-min-long/8307513/Test.java
+++ bugs/intrinsify-max-min-long/8307513/Test.java
@@ -3,62 +3,33 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class Test
 {
-    static final int RANGE = 16*1024;
+    static final int RANGE = 1024;
+    static final int ITER = 10_000;
 
-    public static void main(String[] args)
-    {
-        int iter_warmup = 2_000;
-        int iter_perf = 100_000;
-
-        long[] aLong = new long[RANGE];
-        long[] bLong = new long[RANGE];
-        long[] cLong = new long[RANGE];
-
-        long start, stop;
-
-        long startLongMax = init(aLong, bLong, cLong);
-        long goldLongMax = testLongMax(aLong, bLong, cLong, startLongMax);
-        for (int j = 0; j < iter_warmup; j++)
-        {
-            long total = testLongMax(aLong, bLong, cLong, startLongMax);
-            verify("long max", total, goldLongMax);
-        }
-        start = System.currentTimeMillis();
-        for (int j = 0; j < iter_perf; j++)
-        {
-            testLongMax(aLong, bLong, cLong, startLongMax);
-        }
-        stop = System.currentTimeMillis();
-        System.out.println("long max   " + (stop - start));
-    }
-
-    static long testLongMax(long[] a, long[] b, long[] c, long total)
+    static void init(int[] data)
     {
         for (int i = 0; i < RANGE; i++)
         {
-            long v = (a[i] * b[i]) + (a[i] * c[i]) + (b[i] * c[i]);
-            total = Math.max(total, v);
+            data[i] = i + 1;
         }
-        return total;
     }
 
-    static long init(long[] a, long[] b, long[] c)
+    static int test(int[] data, int sum)
     {
-        final ThreadLocalRandom rand = ThreadLocalRandom.current();
-        for (int j = 0; j < RANGE; j++)
+        for (int i = 0; i < RANGE; i++)
         {
-            a[j] = rand.nextLong();
-            b[j] = rand.nextLong();
-            c[j] = rand.nextLong();
+            sum += 11 * data[i];
         }
-        return rand.nextLong();
+        return sum;
     }
 
-    static void verify(String context, long total, long gold)
+    public static void main(String[] args)
     {
-        if (total != gold)
+        int[] data = new int[RANGE];
+        init(data);
+        for (int i = 0; i < ITER; i++)
         {
-            throw new RuntimeException("Wrong result for " + context + ": " + total + " != " + gold);
+            test(data, i);
         }
     }
 }
