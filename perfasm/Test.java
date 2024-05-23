262e4db35ccec5d4a74fbfeade3b9d3f1165ac7e
Galder Zamarreño <galder@zamarreno.com>
2024-05-23 10:48:49 +0200

    Add example to trigger compilation

---
+++ perfasm/Test.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        final int size = 10;
+        final int[] ints = new int[size];
+        for (int i = 0; i < ints.length; i++)
+        {
+            ints[i] = ThreadLocalRandom.current().nextInt();
+        }
+
+        for (int i = 0; i < 10_000; i++)
+        {
+            int[] result = test(ints);
+            System.out.println(Arrays.toString(result));
+            blackhole(result);
+        }
+    }
+
+    static int[] test(int[] ints)
+    {
+        return ints.clone();
+    }
+
+    static void blackhole(Object obj)
+    {
+        if (obj.hashCode() == System.nanoTime())
+        {
+            System.out.println(obj);
+        }
+    }
+}
