ec6d2c8ed4672d529098e830c9d5c36ad3a5e151
Galder Zamarreño <galder@zamarreno.com>
2024-09-12 18:30:25 +0200

    Fix folder names

--- bugs/intrinsify-max-min-long/8307513/bench.sh
+++ bugs/intrinsify-max-min-long/8307513/bench.sh
@@ -31,8 +31,8 @@ bench()
 
 for jdk_home in \
     "$HOME/1/jdk" \
-    "$HOME/1/jdk/jdk-intrinsify-max-min-long.intrinsic" \
-    "$HOME/1/jdk/jdk-intrinsify-max-min-long"
+    "$HOME/1/jdk-intrinsify-max-min-long.intrinsic" \
+    "$HOME/1/jdk-intrinsify-max-min-long"
 do
     bench $jdk_home
 done
