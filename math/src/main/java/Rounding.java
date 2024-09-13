8bc271a95221aac955bd71bb7d37a30d2e4be57c
Galder Zamarreño <galder@zamarreno.com>
2024-09-13 16:01:03 +0200

    Rounding examples

--- math/src/main/java/Rounding.java
+++ math/src/main/java/Rounding.java
@@ -2,13 +2,25 @@ public class Rounding
 {
     public static void main(String[] args)
     {
-        final int size = 1000;
-        final int aboveCount = 506;
-        final int abovePercent = ((aboveCount + 1) * 100) / size;
-        System.out.println(abovePercent);
+        {
+            final int size = 1000;
+            final int aboveCount = 506;
+            final int abovePercent = ((aboveCount + 1) * 100) / size;
+            System.out.println(abovePercent);
+        }
 
-        final int below = 493;
-        final int belowPercent = below * 100 / size;
-        System.out.println(belowPercent);
+        {
+            final int size = 1000;
+            final int below = 493;
+            final int belowPercent = below * 100 / size;
+            System.out.println(belowPercent);
+        }
+
+        {
+            final int size = 10;
+            final int aboveCount = 7;
+            final int abovePercent = (aboveCount + 1) * 100 / size;
+            System.out.println(abovePercent);
+        }
     }
 }
