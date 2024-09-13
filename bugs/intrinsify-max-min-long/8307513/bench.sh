4a5600b68c5b6aac864beddc7b0652337a0a76e5
Galder Zamarreño <galder@zamarreno.com>
2024-09-13 18:36:25 +0200

    Add option to avoid cleaning

--- bugs/intrinsify-max-min-long/8307513/bench.sh
+++ bugs/intrinsify-max-min-long/8307513/bench.sh
@@ -2,18 +2,34 @@
 
 set -e -x
 
+CLEAN="true"
+
 if [ -z "$TEST" ]; then
     echo "TEST is not defined"
     exit 1
 fi
 
+# Check for clean parameter
+if [[ "$1" == "--clean=false" ]]; then
+    read -p "Are you sure you want to run without cleaning first? (yes/no): " RESPONSE
+    if [[ "$RESPONSE" == "yes" ]]; then
+        CLEAN="false"
+    else
+        echo "Exiting because you don't want to apply the changes."
+        exit 1
+    fi
+fi
+
 bench()
 {
-    local jdk_home=$1
+    local clean=$1
+    local jdk_home=$2
 
-    CONF=release \
-      JDK_HOME=$jdk_home \
-      make configure clean-jdk
+    if [[ $clean == "true" ]]; then
+      CONF=release \
+        JDK_HOME=$jdk_home \
+        make configure clean-jdk
+    fi
 
     CONF=release \
       TEST="micro:lang.$TEST" \
@@ -33,5 +49,5 @@ for jdk_home in \
     "$HOME/1/jdk-intrinsify-max-min-long.intrinsic" \
     "$HOME/1/jdk"
 do
-    bench $jdk_home
+    bench $CLEAN $jdk_home
 done
