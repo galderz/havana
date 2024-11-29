de526b87f37566c8a62666371b65f1755f46f0d8
Galder Zamarreño <galder@zamarreno.com>
2024-11-29 16:11:27 +0100

    Add scalar replacement JMH benchmark

---
+++ jmh-examples/src/main/java/org/sample/ScalarReplacement.java
@@ -0,0 +1,56 @@
+package org.sample;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class ScalarReplacement
+{
+    int x;
+    boolean flag;
+
+    @Setup(Level.Iteration)
+    public void shake()
+    {
+        flag = ThreadLocalRandom.current().nextBoolean();
+    }
+
+    @Benchmark
+    public int single()
+    {
+        MyObject o = new MyObject(x);
+        return o.x;
+    }
+
+    @Benchmark
+    public int split()
+    {
+        MyObject o;
+        if (flag)
+        {
+            o = new MyObject(x);
+        }
+        else
+        {
+            o = new MyObject(x);
+        }
+        return o.x;
+    }
+
+    static class MyObject
+    {
+        final int x;
+
+        public MyObject(int x)
+        {
+            this.x = x;
+        }
+    }
+}
