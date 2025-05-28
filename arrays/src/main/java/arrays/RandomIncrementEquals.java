378f1f7b07acc9a6804226484cfa3e75de01e6f8
Galder Zamarreño <galder@zamarreno.com>
2025-05-28 07:50:03 +0200

    Fix random increment equals logic

--- arrays/src/main/java/arrays/RandomIncrementEquals.java
+++ arrays/src/main/java/arrays/RandomIncrementEquals.java
@@ -5,10 +5,12 @@ import java.util.stream.LongStream;
 
 public class RandomIncrementEquals
 {
-    static long[][] distributeLongRandomIncrement(int size, int probability)
+    static long[][] distributeLongRandomIncrement(int size, int probability, boolean includeEquals)
     {
+        final long aboveOrEqualsLowerBound = includeEquals ? 0 : 1;
+
         long[][] result;
-        int aboveCount, abovePercent;
+        int aboveOrEqualsCount, aboveOrEqualsPercent;
 
         // This algorithm generates 2 arrays of numbers.
         // The first array is created such that as the array is iterated,
@@ -26,29 +28,40 @@ public class RandomIncrementEquals
             result[0][0] = max;
             result[1][0] = max - 1;
 
-            aboveCount = 0;
+            // Assume that the first value is above or equals to the current max
+            aboveOrEqualsCount = 1;
             for (int i = 1; i < result[0].length; i++)
             {
                 long value;
-                if (ThreadLocalRandom.current().nextLong(101) <= probability)
+                if (i == 1 && includeEquals)
                 {
-                    long increment = ThreadLocalRandom.current().nextLong(10);
+                    // If include equals make sure there is at least one element that is equals to the current min/max.
+                    // So, in that case make the 2nd element in the array the same as the current min/max.
+                    value = max;
+                    aboveOrEqualsCount++;
+                    // System.out.printf("[generate] value[1] = %d; special includeEquals value, aboveOrEqualsCount = %d%n", value, aboveOrEqualsCount);
+                }
+                else if (ThreadLocalRandom.current().nextLong(101) <= probability)
+                {
+                    long increment = ThreadLocalRandom.current().nextLong(aboveOrEqualsLowerBound, 10);
                     value = max + increment;
-                    aboveCount++;
+                    aboveOrEqualsCount++;
+                    // System.out.printf("[generate] value[%d] = %d; above or equals, aboveOrEqualsCount = %d%n", i, value, aboveOrEqualsCount);
                 }
                 else
                 {
-                    // Decrement by at least 1
-                    long diffToMax = ThreadLocalRandom.current().nextLong(10) + 1;
+                    // To generate a value strictly lower value, it must generate at least 1 element lower
+                    long diffToMax = ThreadLocalRandom.current().nextLong(1, 10);
                     value = max - diffToMax;
+                    // System.out.printf("[generate] value[%d] = %d; below, aboveOrEqualsCount = %d%n", i, value, aboveOrEqualsCount);
                 }
                 result[0][i] = value;
                 result[1][i] = max;
                 max = Math.max(max, value);
             }
 
-            abovePercent = ((aboveCount + 1) * 100) / size;
-        } while (abovePercent != probability);
+            aboveOrEqualsPercent = (aboveOrEqualsCount * 100) / size;
+        } while (aboveOrEqualsPercent != probability);
 
         return result;
     }
@@ -56,136 +69,208 @@ public class RandomIncrementEquals
     public static void main(String[] args)
     {
         Asserts.needEnabledAsserts();
-
-        testMinReduction(negate(distributeLongRandomIncrement(10, 50)[0]), 50);
-        testMinReduction(negate(distributeLongRandomIncrement(10, 60)[0]), 60);
-        testMinReduction(negate(distributeLongRandomIncrement(10, 80)[0]), 80);
-        testMinReduction(negate(distributeLongRandomIncrement(10, 100)[0]), 100);
-
-        testMaxReduction(distributeLongRandomIncrement(10, 50)[0], 50);
-        testMaxReduction(distributeLongRandomIncrement(10, 60)[0], 60);
-        testMaxReduction(distributeLongRandomIncrement(10, 80)[0], 80);
-        testMaxReduction(distributeLongRandomIncrement(10, 100)[0], 100);
-
-        testMaxReduction(distributeLongRandomIncrement(100, 50)[0], 50);
-        testMaxReduction(distributeLongRandomIncrement(100, 60)[0], 60);
-        testMaxReduction(distributeLongRandomIncrement(100, 75)[0], 75);
-        testMaxReduction(distributeLongRandomIncrement(100, 80)[0], 80);
-        testMaxReduction(distributeLongRandomIncrement(100, 100)[0], 100);
-
-        testMaxReduction(distributeLongRandomIncrement(1000, 50)[0], 50);
-        testMaxReduction(distributeLongRandomIncrement(1000, 60)[0], 60);
-        testMaxReduction(distributeLongRandomIncrement(1000, 75)[0], 75);
-        testMaxReduction(distributeLongRandomIncrement(1000, 80)[0], 80);
-        testMaxReduction(distributeLongRandomIncrement(1000, 100)[0], 100);
-
-        testMaxReduction(distributeLongRandomIncrement(10000, 50)[0], 50);
-        testMaxReduction(distributeLongRandomIncrement(10000, 60)[0], 60);
-        testMaxReduction(distributeLongRandomIncrement(10000, 75)[0], 75);
-        testMaxReduction(distributeLongRandomIncrement(10000, 80)[0], 80);
-        testMaxReduction(distributeLongRandomIncrement(10000, 100)[0], 100);
-
-        testMaxLoop(distributeLongRandomIncrement(10, 50), 50);
-        testMaxLoop(distributeLongRandomIncrement(10, 60), 60);
-        testMaxLoop(distributeLongRandomIncrement(10, 80), 80);
-        testMaxLoop(distributeLongRandomIncrement(10, 100), 100);
-
-        testMaxLoop(distributeLongRandomIncrement(100, 50), 50);
-        testMaxLoop(distributeLongRandomIncrement(100, 60), 60);
-        testMaxLoop(distributeLongRandomIncrement(100, 75), 75);
-        testMaxLoop(distributeLongRandomIncrement(100, 80), 80);
-        testMaxLoop(distributeLongRandomIncrement(100, 100), 100);
-
-        testMaxLoop(distributeLongRandomIncrement(1000, 50), 50);
-        testMaxLoop(distributeLongRandomIncrement(1000, 60), 60);
-        testMaxLoop(distributeLongRandomIncrement(1000, 75), 75);
-        testMaxLoop(distributeLongRandomIncrement(1000, 80), 80);
-        testMaxLoop(distributeLongRandomIncrement(1000, 100), 100);
-
-        testMaxLoop(distributeLongRandomIncrement(10000, 50), 50);
-        testMaxLoop(distributeLongRandomIncrement(10000, 60), 60);
-        testMaxLoop(distributeLongRandomIncrement(10000, 75), 75);
-        testMaxLoop(distributeLongRandomIncrement(10000, 80), 80);
-        testMaxLoop(distributeLongRandomIncrement(10000, 100), 100);
+        new Test(true).test();
+        new Test(false).test();
     }
 
-    private static void testMaxLoop(long[][] nums, int expectedAboveOrEqualMax)
+    private static class Test
+    {
+        final boolean includesEquals;
+
+        private Test(boolean includesEquals)
+        {
+            this.includesEquals = includesEquals;
+        }
+
+        void test()
+        {
+            testMinReduction(negate(distributeLongRandomIncrement(10, 50, includesEquals)[0]), 50, includesEquals);
+            testMinReduction(negate(distributeLongRandomIncrement(10, 60, includesEquals)[0]), 60, includesEquals);
+            testMinReduction(negate(distributeLongRandomIncrement(10, 80, includesEquals)[0]), 80, includesEquals);
+            testMinReduction(negate(distributeLongRandomIncrement(10, 100, includesEquals)[0]), 100, includesEquals);
+
+            testMaxReduction(distributeLongRandomIncrement(10, 50, includesEquals)[0], 50, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10, 60, includesEquals)[0], 60, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10, 80, includesEquals)[0], 80, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10, 100, includesEquals)[0], 100, includesEquals);
+
+            testMaxReduction(distributeLongRandomIncrement(100, 50, includesEquals)[0], 50, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(100, 60, includesEquals)[0], 60, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(100, 75, includesEquals)[0], 75, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(100, 80, includesEquals)[0], 80, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(100, 100, includesEquals)[0], 100, includesEquals);
+
+            testMaxReduction(distributeLongRandomIncrement(1000, 50, includesEquals)[0], 50, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(1000, 60, includesEquals)[0], 60, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(1000, 75, includesEquals)[0], 75, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(1000, 80, includesEquals)[0], 80, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(1000, 100, includesEquals)[0], 100, includesEquals);
+
+            testMaxReduction(distributeLongRandomIncrement(10000, 50, includesEquals)[0], 50, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10000, 60, includesEquals)[0], 60, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10000, 75, includesEquals)[0], 75, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10000, 80, includesEquals)[0], 80, includesEquals);
+            testMaxReduction(distributeLongRandomIncrement(10000, 100, includesEquals)[0], 100, includesEquals);
+
+            testMaxLoop(distributeLongRandomIncrement(10, 50, includesEquals), 50, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10, 60, includesEquals), 60, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10, 80, includesEquals), 80, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10, 100, includesEquals), 100, includesEquals);
+
+            testMaxLoop(distributeLongRandomIncrement(100, 50, includesEquals), 50, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(100, 60, includesEquals), 60, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(100, 75, includesEquals), 75, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(100, 80, includesEquals), 80, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(100, 100, includesEquals), 100, includesEquals);
+
+            testMaxLoop(distributeLongRandomIncrement(1000, 50, includesEquals), 50, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(1000, 60, includesEquals), 60, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(1000, 75, includesEquals), 75, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(1000, 80, includesEquals), 80, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(1000, 100, includesEquals), 100, includesEquals);
+
+            testMaxLoop(distributeLongRandomIncrement(10000, 50, includesEquals), 50, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10000, 60, includesEquals), 60, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10000, 75, includesEquals), 75, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10000, 80, includesEquals), 80, includesEquals);
+            testMaxLoop(distributeLongRandomIncrement(10000, 100, includesEquals), 100, includesEquals);
+        }
+    }
+
+    private static void testMaxLoop(long[][] nums, int expectedAboveOrEqualMax, boolean includesEquals)
     {
         final long[] a = nums[0];
         final long[] b = nums[1];
         int above = 0;
+        int equals = 0;
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] >= b[i]) {
-                above++;
-                // System.out.printf("a[%d] = %d, b[%d] = %d, a side%n", i, a[i], i, b[i]);
+            if (a[i] == b[i])
+            {
+                // System.out.printf("a[%d] = %d, b[%d] = %d, equals%n", i, a[i], i, b[i]);
+                equals++;
             }
-            if (a[i] < b[i]) {
+            else if (a[i] > b[i])
+            {
+                // System.out.printf("a[%d] = %d, b[%d] = %d, a side%n", i, a[i], i, b[i]);
+                above++;
+            }
+            else
+            {
                 // System.out.printf("a[%d] = %d, b[%d] = %d, b side%n", i, a[i], i, b[i]);
             }
-
         }
 
-        int aboveOrEqualMaxPercentage = (above * 100) / a.length;
+        final int aboveOrEquals = above + equals;
+        int aboveOrEqualMaxPercentage = (aboveOrEquals * 100) / a.length;
         int belowMaxPercentage = 100 - aboveOrEqualMaxPercentage;
 
-        System.out.printf("Percentage above or equal max value: %d%% from above %d and and array size %d%n", aboveOrEqualMaxPercentage, above, a.length);
+        System.out.printf("Percentage above or equal max value: %d%% from above %d and and array size %d%n", aboveOrEqualMaxPercentage, aboveOrEquals, a.length);
         System.out.printf("Percentage below to max value: %d%%%n", belowMaxPercentage);
 
         assert aboveOrEqualMaxPercentage == expectedAboveOrEqualMax : String.format("Expected %d%% above or equal max but got %d%%", expectedAboveOrEqualMax, aboveOrEqualMaxPercentage);
+        if (includesEquals)
+        {
+            assert equals > 0 : "Expected equals to be bigger than zero but was zero";
+        }
+        else
+        {
+            assert equals == 0 : String.format("Expected equals to be zero but was %d", equals);
+        }
     }
 
-    private static void testMaxReduction(long[] nums, int expectedAboveOrEqualMax)
+    private static void testMaxReduction(long[] nums, int expectedAboveOrEqualMax, boolean includesEquals)
     {
         long max = Long.MIN_VALUE;
         int above = 0;
+        int equals = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++){
             final long v = 11 * nums[i];
-            if (v >= max) {
-                above++;
-                max = v;
-                // System.out.println("Element: " + nums[i] + ", Current Max: " + max + ", Max");
+            if (v == max)
+            {
+                System.out.println("Element: " + v + ", Current Max: " + max + ", Equals");
+                equals++;
             }
-            if (v < max) {
-                // System.out.println("Element: " + nums[i] + ", Current Max: " + max + ", Below");
+            else if (v > max)
+            {
+                System.out.println("Element: " + v + ", Current Max: " + max + ", Above");
+                above++;
+            }
+            else
+            {
+                System.out.println("Element: " + v + ", Current Max: " + max + ", Below");
+            }
+
+            if (v >= max)
+            {
+                max = v;
             }
         }
 
-        int aboveOrEqualMaxPercentage = (above * 100) / nums.length;
+        final int aboveOrEquals = above + equals;
+        int aboveOrEqualMaxPercentage = (aboveOrEquals * 100) / nums.length;
         int belowMaxPercentage = 100 - aboveOrEqualMaxPercentage;
 
-        System.out.printf("Percentage above or equal max value: %d%% from above %d and and array size %d%n", aboveOrEqualMaxPercentage, above, nums.length);
-        System.out.printf("Percentage below to max value: %d%%%n", belowMaxPercentage);
+        System.out.printf("Percentage above or equal max value: %d%% from above or equals %d and and array size %d%n", aboveOrEqualMaxPercentage, aboveOrEquals, nums.length);
+        System.out.printf("Percentage below max value: %d%%%n", belowMaxPercentage);
 
         assert aboveOrEqualMaxPercentage == expectedAboveOrEqualMax : String.format("Expected %d%% above or equal max but got %d%%", expectedAboveOrEqualMax, aboveOrEqualMaxPercentage);
+        if (includesEquals)
+        {
+            assert equals > 0 : "Expected equals to be bigger than zero but was zero";
+        }
+        else
+        {
+            assert equals == 0 : String.format("Expected equals to be zero but was %d", equals);
+        }
     }
 
-    private static void testMinReduction(long[] nums, int expectedBelowOrEqualMin)
+    private static void testMinReduction(long[] nums, int expectedBelowOrEqualMin, boolean includesEquals)
     {
         long min = Long.MAX_VALUE;
         int below = 0;
+        int equals = 0;
 
         for (int i = 0; i < nums.length; i++) {
             final long v = 11 * nums[i];
-            if (v <= min) {
-                below++;
-                min = v;
-                System.out.println("Element: " + v + ", Current Min: " + min + ", Below");
+            if (v == min)
+            {
+                System.out.println("Element: " + v + ", Current Min: " + min + ", Equals");
+                equals++;
             }
-            if (v > min) {
+            else if (v < min)
+            {
+                System.out.println("Element: " + v + ", Current Min: " + min + ", Below");
+                below++;
+            }
+            else
+            {
                 System.out.println("Element: " + v + ", Current Min: " + min + ", Above");
             }
 
+            if (v <= min)
+            {
+                min = v;
+            }
         }
 
-        int belowOrEqualMinPercentage = (below * 100) / nums.length;
+        final int belowOrEquals = below + equals;
+        int belowOrEqualMinPercentage = (belowOrEquals * 100) / nums.length;
         int aboveMinPercentage = 100 - belowOrEqualMinPercentage;
 
-        System.out.printf("Percentage above or equal max value: %d%% from above %d and and array size %d%n", belowOrEqualMinPercentage, below, nums.length);
-        System.out.printf("Percentage below to max value: %d%%%n", aboveMinPercentage);
+        System.out.printf("Percentage below or equal min value: %d%% from below or equals %d and and array size %d%n", belowOrEqualMinPercentage, belowOrEquals, nums.length);
+        System.out.printf("Percentage above min value: %d%%%n", aboveMinPercentage);
 
         assert belowOrEqualMinPercentage == expectedBelowOrEqualMin : String.format("Expected %d%% above or equal max but got %d%%", expectedBelowOrEqualMin, belowOrEqualMinPercentage);
+        if (includesEquals)
+        {
+            assert equals > 0 : "Expected equals to be bigger than zero but was zero";
+        }
+        else
+        {
+            assert equals == 0 : String.format("Expected equals to be zero but was %d", equals);
+        }
     }
 
     static long[] negate(long[] nums)
